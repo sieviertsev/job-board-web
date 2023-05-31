65147ab01f8798bc2307fcca7f213f438a14b0be
Serhiy <dev11@globalpartnersoft.com>
2023-06-01 00:22:00 +0300

    feat(user): added signUp, signIn and logOut

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('home/', views.home, name = 'home'),
 
     path('user/', include('user.urls')),
 
